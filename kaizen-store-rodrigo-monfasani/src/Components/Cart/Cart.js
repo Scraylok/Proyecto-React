d7de50ae942938001802b1af8f2f5dc4bb010423
Scraylok <rodrigomonfasani@gmail.com>
2022-06-21 21:13:25 -0300

    navbar2

---
+++ kaizen-store-rodrigo-monfasani/src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import shopping_cart from "../../Assests/shopping-cart.png";
+
+
+const Cart = () => {
+    return {
+    
+        <button><img src={shopping_cart} alt="Cart Icon"></button>
+    }
+    
+}
+export default Cart
