2e975b4cf2e31f8ee28ce4ed2456afde93508aa4
Scraylok <rodrigomonfasani@gmail.com>
2022-07-08 11:47:41 -0300

    Routing

--- kaizen-store-rodrigo-monfasani/src/App.jsx
+++ kaizen-store-rodrigo-monfasani/src/App.jsx
@@ -5,22 +5,21 @@ import cart from './Components/Cart/Cart.js'
 import Landing from './Components/Landing/Landing';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import ItemDetailcontainer from './Components/ItemDetailContainer/ItemDetailcontainer';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
    <>
    <BrowserRouter>
       <Navbar/>
+      
       <Routes>
+        <Route path='/' element={<div className="App"><Landing Greeting="Bienvenido a" /></div>}/>
         <Route path='/' element={<ItemListContainer/>} />
-        <Route path='/categoria' element={<ItemListContainer/>} />
+        <Route path='/' element={<ItemListContainer/>} />
         <Route path='/cart' element={ <cart/> } />
-        <Route path='/detalle/:id' element={<ItemDetailcontainer/>} />
+        <Route path='/category/' element={<ItemDetailcontainer/>} />
       </Routes>
-      <div className="App">
-      <Landing Greeting="Bienvenido a" />
-      </div>
    </BrowserRouter>
 
    </>
