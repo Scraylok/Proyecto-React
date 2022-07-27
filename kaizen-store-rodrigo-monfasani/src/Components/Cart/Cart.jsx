fde6a97e437020ba1bfc098e150c6732a0d34fbe
Scraylok <rodrigomonfasani@gmail.com>
2022-07-27 11:37:25 -0300

    fibase v2

--- kaizen-store-rodrigo-monfasani/src/Components/Cart/Cart.jsx
+++ kaizen-store-rodrigo-monfasani/src/Components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../Context/CartContext'
@@ -11,9 +12,15 @@ const Cart = () => {
       email: 'juansito@gmail.com',
       phone: '1231231',
       address: 'calle falsa'
-    }
-    Items: Cart.map(products => ({id: products.id, title: products.title,price: products.price, quantity: products.quantity})),
-    total: totalPrice() ,
+    },
+    items: Cart.map(products => ({ id: products.id, title: products.title, price: products.price, quantity: products.quantity,})),
+    total: totalPrice(),
+  }
+  const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db,'orders');
+    addDoc[ordersCollection,order]
+    .then(({id}) =>console.log(id))
   }
 
   if (Cart.lenght === 0){
@@ -31,7 +38,7 @@ const Cart = () => {
         Cart.map(products => <itemCart key={products.id} products={products}/>)
       }
       <p>Total: {totalPrice()}</p>
-      <button>Comprar</button>
+      <button onClick={handleClick}>Comprar</button>
     </>
   )
 }
